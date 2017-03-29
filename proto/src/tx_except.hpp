a0af5065def1b18e4dbfff74bf3fdeb4c81323d7
Christer Swahn <christer.e.swahn@gmail.com>
2017-03-29 15:01:45 +0200

    (added new file)

---
+++ proto/src/tx_except.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+
+#include <stdexcept>
+
+
+class TxParseOrigin;
+
+
+/** Represents a failure to resolve a type / field.
+ * (This is used instead of passing NULL values up the call stack, requiring many NULL pointer checks.)
+ * It is expected that the original throwing site will have generated an appropriate compiler error or log message.
+ */
+class resolution_error : public std::runtime_error {
+    TxParseOrigin const * const origin;
+public:
+    resolution_error( const TxParseOrigin* origin, const std::string& errMessage )
+        : std::runtime_error( errMessage ), origin( origin )  { }
+};
+
+
+template<typename charT, typename traits>
+std::basic_ostream<charT, traits> &
+operator<< (std::basic_ostream<charT, traits> &lhs, const resolution_error& rhs) {
+    return lhs << rhs.what();
+}
+
+
+#define THROW_LOGIC(message) \
+do { \
+    std::stringstream msg;  msg << message; \
+    throw std::logic_error( msg.str() ); \
+} while (false)
